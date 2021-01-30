3f331a6e6496ba6219f24b8ade3083fd1c7129c4
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 15:56:54 +0800

    Added controllers and routes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// import all routes
+const products = require("./routes/product.routes");
+
+app.use("/api/v1", products);
+
 module.exports = app;
