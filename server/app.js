be405f8d01c9b82051e99302d4d872c23d034a0a
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 21:20:53 +0800

    Encrypt password before registration

--- server/app.js
+++ server/app.js
@@ -3,12 +3,13 @@ const errorMiddleware = require("./middlewares/errors");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
 
 // import all routes
 const products = require("./routes/product.routes");
+const users = require("./routes/user.routes");
 
 app.use("/api/v1", products);
+app.use("/api/v1", users);
 
 // Errorhandler middleware
 app.use(errorMiddleware);
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
 
@@ -41,4 +42,12 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Encrypt password before saving the user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
 module.exports = mongoose.model("User", userSchema);
