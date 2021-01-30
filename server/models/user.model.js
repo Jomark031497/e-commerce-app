422a9be8b961c53986fb442f9dc87297f8a25751
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-01-30 23:24:47 +0800

    Generate forgot password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -62,5 +63,21 @@ userSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: 3600 * 24 * 1,
   });
+};
+
+// Generate reset password token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  // Hash and set to resetPasswordToken
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set token expire time
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+  return resetToken;
 };
 module.exports = mongoose.model("User", userSchema);
