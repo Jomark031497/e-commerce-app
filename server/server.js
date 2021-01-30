7dc19cc8d01ae927d59ef23f12ab70b2c60f3393
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 18:38:27 +0800

    Added exception handlers for uncaught exceptions and promise rejections

--- server/server.js
+++ server/server.js
@@ -2,14 +2,31 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down server due to uncaught exceptions");
+  process.exit(1);
+});
+
 // Setting up config file
 dotenv.config({ path: "server/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("shutting down server due to unhandled promise rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
