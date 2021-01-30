63b7368f9fa6e573c45631dc2d88624217948e6c
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 17:53:54 +0800

    Added error handler middleware

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorMiddleware = require("./middlewares/errors");
 const app = express();
 
 app.use(express.json());
@@ -7,5 +8,8 @@ app.use(express.json());
 const products = require("./routes/product.routes");
 
 app.use("/api/v1", products);
+
+// Errorhandler middleware
+app.use(errorMiddleware);
 
 module.exports = app;
--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product.model");
+const ErrorHandler = require("../utils/errorHandler");
 
 // DESC:  CREATE A NEW PRODUCT
 // ROUTE: /api/v1/product/new
@@ -20,10 +21,7 @@ exports.getProducts = async (req, res, next) => {
 exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
-  if (!product)
-    return res
-      .status(404)
-      .json({ success: false, message: "Product not found" });
+  if (!product) return next(new ErrorHandler("Product not found", 404));
 
   res.status(200).json({ success: true, product });
 };
