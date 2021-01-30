4754fe05a6dd1a63aa3c624a161b6a7869839b28
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-01-31 00:18:40 +0800

    Handled JWT errors & mongoose duplicate key errors

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -29,6 +29,12 @@ module.exports = (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    // Handling mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: err.message || "Internal Server Error",
