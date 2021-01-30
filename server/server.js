98d46dea066a10a51d2104e399ce00224906f4c2
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 15:40:26 +0800

    Setup server and configs

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+
+// Setting up config file
+dotenv.config({ path: "server/config/config.env" });
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
--- server/src/routes/productRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const { getProducts } = require("../controllers/productController");
-
-router.get("/products", getProducts);
-
-module.exports = router;
