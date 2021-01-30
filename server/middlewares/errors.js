e7f7f0cac8636a74845bd001b05eba99be48a330
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-30 18:48:56 +0800

    Added mongoose validation error handler

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -17,11 +17,21 @@ module.exports = (err, req, res, next) => {
 
     error.message = err.message;
 
-    res
-      .status(err.statusCode)
-      .json({
-        success: false,
-        message: err.message || "Internal Server Error",
-      });
+    // Wrong mongoose Ojbect ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling mongoose validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message || "Internal Server Error",
+    });
   }
 };
