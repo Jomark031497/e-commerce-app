b342bd2111ad32317531b13678b99073f667fa2a
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-01-31 14:51:48 +0800

    Get single order and loggedin user orders

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { newOrder } = require("../controllers/order.controller");
+const {
+  newOrder,
+  myOrders,
+  getSingleOrder,
+} = require("../controllers/order.controller");
 
 const { isAuthenticated, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthenticated, newOrder);
+
+router.route("/order/:id").get(isAuthenticated, getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticated, myOrders);
 
 module.exports = router;
